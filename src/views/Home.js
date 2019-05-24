0f247b4c21ace87badd81add80b30605ffb67cb9
Patricia Montoya <patmonhin@gmail.com>
2019-05-23 22:47:51 -0500

    Adding data from API

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import moviesData from '../data/movies.json'
 import MovieCard from '../components/MovieCard'
 
+const API_KEY = '298ad9ee38c9159771659fe5a01b1dc7'
+
 class Home extends React.Component {
   state = {
-   ...moviesData,
+   ...moviesData
   }
 
   deleteMovie = (movieId) => {
@@ -16,13 +18,19 @@ class Home extends React.Component {
     })
   }
 
+  componentDidMount() {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then((res) => res.json())
+    .then((data) => {this.setState({ movies : data.results })})
+  }
 
     render() {
         const { movies } = this.state
+        const popularMovies = movies.slice(0,5)
         return <div>
         <h1 className='main-title'>Movie App</h1>
         <div className='content'>
-          {movies.map((movie) => <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />)}
+          {popularMovies.map((movie) => <MovieCard deleteMovie={this.deleteMovie} key={movie.id} {...movie} />)}
         </div>
       </div>
     }
